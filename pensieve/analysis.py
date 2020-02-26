44ef1ba753440c1c1117cb675173d958d93db380
Anna Scholtz <anna@scholtzan.net>
2020-02-26 14:40:33 -0800

    WIP fetch recipes for experiments and run query

--- pensieve/analysis.py
+++ pensieve/analysis.py
@@ -11,7 +11,7 @@ from pensieve.experimenter import Variant
 
 class Analysis:
     # todo: where should this come from? experiments, metrics?
-    ANALYSIS_PERIOD = 7  # 1 week
+    ANALYSIS_PERIOD = 1  # 1 week
 
     # list of standard to be computed
     STANDARD_METRICS = [
@@ -28,6 +28,9 @@ class Analysis:
         self.bq_context = BigQueryContext(project_id=project, dataset_id=dataset)
 
     def run(self, experiment):
+        if experiment.recipe is None:
+            return # for some reason some experiment recipes don't include the slug, this might be a bug?
+
         # check if experiment should be analysed
         # todo: can UTC be assumed?
         current_date = datetime.combine(datetime.today(), datetime.min.time()).replace(
@@ -36,11 +39,10 @@ class Analysis:
         date_delta = current_date - experiment.start_date
         next_analysis_date = current_date + timedelta(days=date_delta.days % self.ANALYSIS_PERIOD)
 
-        if current_date == next_analysis_date or experiment.end_date < current_date:
+        if (date_delta.days > 0 and current_date == next_analysis_date) or experiment.end_date < current_date:
             exp = Experiment(
-                experiment_slug=experiment.slug,
-                start_date=experiment.start_date,
-                num_dates_enrollment=14,  # todo
+                experiment_slug=experiment.recipe.slug,
+                start_date=experiment.start_date.strftime('%Y-%m-%d')
             )
 
             last_date_full_data = current_date
@@ -50,10 +52,11 @@ class Analysis:
 
             # todo additional experiment specific metrics from Experimenter in addition to standard ones
             # todo output to custom table
-            # ts_res = exp.get_single_window_data(
-            #     bq_context=self.bq_context,
-            #     metric_list=self.STANDARD_METRICS,
-            #     last_date_full_data=last_date_full_data,
-            #     analysis_start_days=max(0, date_delta - self.ANALYSIS_PERIOD),
-            #     analysis_length_days=self.ANALYSIS_PERIOD
-            # )
+            # todo append to existing table
+            ts_res = exp.get_single_window_data(
+                bq_context=self.bq_context,
+                metric_list=self.STANDARD_METRICS,
+                last_date_full_data=last_date_full_data.strftime('%Y-%m-%d'),
+                analysis_start_days=max(0, date_delta.days - self.ANALYSIS_PERIOD),
+                analysis_length_days=self.ANALYSIS_PERIOD
+            )
