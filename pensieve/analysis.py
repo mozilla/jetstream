c5be2111e7fe52c796c0254ddfcf52e93ddf781f
Anna Scholtz <anna@scholtzan.net>
2020-02-27 13:06:15 -0800

    Use normandy_slug fetched from Experimenter for mozanalysis

--- pensieve/analysis.py
+++ pensieve/analysis.py
@@ -1,17 +1,13 @@
 import mozanalysis.metrics.desktop as mmd
-import mozanalysis.bayesian_stats.binary as mabsbin
-import attr
 from mozanalysis.experiment import Experiment
 from mozanalysis.bq import BigQueryContext
 from datetime import datetime, timedelta
 import pytz
 
-from pensieve.experimenter import Variant
-
 
 class Analysis:
     # todo: where should this come from? experiments, metrics?
-    ANALYSIS_PERIOD = 1  # 1 week
+    ANALYSIS_PERIOD = 7  # 1 week
 
     # list of standard to be computed
     STANDARD_METRICS = [
@@ -28,9 +24,6 @@ class Analysis:
         self.bq_context = BigQueryContext(project_id=project, dataset_id=dataset)
 
     def run(self, experiment):
-        if experiment.recipe is None:
-            return # for some reason some experiment recipes don't include the slug, this might be a bug?
-
         # check if experiment should be analysed
         # todo: can UTC be assumed?
         current_date = datetime.combine(datetime.today(), datetime.min.time()).replace(
@@ -39,10 +32,12 @@ class Analysis:
         date_delta = current_date - experiment.start_date
         next_analysis_date = current_date + timedelta(days=date_delta.days % self.ANALYSIS_PERIOD)
 
-        if (date_delta.days > 0 and current_date == next_analysis_date) or experiment.end_date < current_date:
+        if (
+            date_delta.days > 0 and current_date == next_analysis_date
+        ) or experiment.end_date < current_date:
             exp = Experiment(
-                experiment_slug=experiment.recipe.slug,
-                start_date=experiment.start_date.strftime('%Y-%m-%d')
+                experiment_slug=experiment.normandy_slug,
+                start_date=experiment.start_date.strftime("%Y-%m-%d"),
             )
 
             last_date_full_data = current_date
@@ -50,13 +45,13 @@ class Analysis:
             if experiment.end_date < current_date:
                 last_date_full_data = experiment.end_date
 
-            # todo additional experiment specific metrics from Experimenter in addition to standard ones
+            # todo additional experiment specific metrics from Experimenter
             # todo output to custom table
             # todo append to existing table
-            ts_res = exp.get_single_window_data(
+            exp.get_single_window_data(
                 bq_context=self.bq_context,
                 metric_list=self.STANDARD_METRICS,
-                last_date_full_data=last_date_full_data.strftime('%Y-%m-%d'),
+                last_date_full_data=last_date_full_data.strftime("%Y-%m-%d"),
                 analysis_start_days=max(0, date_delta.days - self.ANALYSIS_PERIOD),
-                analysis_length_days=self.ANALYSIS_PERIOD
+                analysis_length_days=self.ANALYSIS_PERIOD,
             )
