bfc272cb27d6fb344ebb0f8dd196a6a821add18c
Anna Scholtz <anna@scholtzan.net>
2020-03-05 13:00:32 -0800

    Add analysis tests

--- pensieve/analysis.py
+++ pensieve/analysis.py
@@ -6,10 +6,14 @@ import pytz
 
 
 class Analysis:
+    """
+    Wrapper for analysing experiments.
+    """
+
     # todo: where should this come from? experiments, metrics?
     ANALYSIS_PERIOD = 7  # 1 week
 
-    # list of standard to be computed
+    # list of standard metrics to be computed
     STANDARD_METRICS = [
         mmd.active_hours,
         mmd.uri_count,
@@ -20,35 +24,45 @@ class Analysis:
     def __init__(self, project, dataset):
         self.project = project
         self.dataset = dataset
-
         self.bq_context = BigQueryContext(project_id=project, dataset_id=dataset)
 
-    def run(self, experiment):
-        if experiment.normandy_slug is None:
-            return  # some experiments do not have a normandy slug
+    def _current_date(self):
+        """Returns the current date with UTC timezone and time set to 00:00:00."""
+        return datetime.combine(datetime.today(), datetime.min.time()).replace(tzinfo=pytz.utc)
 
-        # check if experiment should be analysed
-        # todo: can UTC be assumed?
-        current_date = datetime.combine(datetime.today(), datetime.min.time()).replace(
-            tzinfo=pytz.utc
-        )
+    def _should_analyse_experiment(self, experiment, current_date):
+        """
+        Returns True if a passed experiment should be analysed based
+        on its start date and the last time it got analysed.
+        """
         date_delta = current_date - experiment.start_date
         next_analysis_date = current_date + timedelta(days=date_delta.days % self.ANALYSIS_PERIOD)
 
-        if (
+        return (
             date_delta.days > 0 and current_date == next_analysis_date
-        ) or experiment.end_date < current_date:
+        ) or experiment.end_date < current_date
+
+    def run(self, experiment):
+        """
+        Run analysis using mozanalysis for a specific experiment.
+        """
+
+        if experiment.normandy_slug is None:
+            return  # some experiments do not have a normandy slug
+
+        current_date = self._current_date()
+
+        if self._should_analyse_experiment(experiment, current_date):
             exp = Experiment(
                 experiment_slug=experiment.normandy_slug,
                 start_date=experiment.start_date.strftime("%Y-%m-%d"),
             )
 
+            date_delta = current_date - experiment.start_date
             last_date_full_data = current_date
 
             if experiment.end_date < current_date:
                 last_date_full_data = experiment.end_date
-
-            print(experiment.normandy_slug)
 
             # todo additional experiment specific metrics from Experimenter
             # todo output to custom table
