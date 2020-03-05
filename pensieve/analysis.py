e65d62e29b38f2e7789937c7309cd279a306e48b
Anna Scholtz <anna@scholtzan.net>
2020-03-05 13:50:55 -0800

    Write results to BigQuery table

--- pensieve/analysis.py
+++ pensieve/analysis.py
@@ -1,8 +1,9 @@
 import mozanalysis.metrics.desktop as mmd
-from mozanalysis.experiment import Experiment
+from mozanalysis.experiment import Experiment, TimeLimits
 from mozanalysis.bq import BigQueryContext
 from datetime import datetime, timedelta
 import pytz
+import re
 
 
 class Analysis:
@@ -42,10 +43,26 @@ class Analysis:
             date_delta.days > 0 and current_date == next_analysis_date
         ) or experiment.end_date < current_date
 
+    # copied from mozanalyis
+    # https://github.com/mozilla/mozanalysis/blob/579119e2f34a9259d5a79d03d86cea89eda34280/src/mozanalysis/bq.py#L9
+    def _sanitize_table_name_for_bq(self, table_name):
+        of_good_character_but_possibly_verbose = re.sub(r"[^a-zA-Z_0-9]", "_", table_name)
+
+        if len(of_good_character_but_possibly_verbose) <= 1024:
+            return of_good_character_but_possibly_verbose
+
+        return (
+            of_good_character_but_possibly_verbose[:500]
+            + "___"
+            + of_good_character_but_possibly_verbose[-500:]
+        )
+
     def run(self, experiment):
         """
         Run analysis using mozanalysis for a specific experiment.
         """
+
+        print(experiment.normandy_slug)
 
         if experiment.normandy_slug is None:
             return  # some experiments do not have a normandy slug
@@ -64,13 +81,22 @@ class Analysis:
             if experiment.end_date < current_date:
                 last_date_full_data = experiment.end_date
 
-            # todo additional experiment specific metrics from Experimenter
-            # todo output to custom table
-            # todo append to existing table
-            exp.get_single_window_data(
-                bq_context=self.bq_context,
-                metric_list=self.STANDARD_METRICS,
-                last_date_full_data=last_date_full_data.strftime("%Y-%m-%d"),
-                analysis_start_days=max(0, date_delta.days - self.ANALYSIS_PERIOD),
-                analysis_length_days=self.ANALYSIS_PERIOD,
+            # build and execute the BigQuery query
+            last_date_full_data = last_date_full_data.strftime("%Y-%m-%d")
+            analysis_start_days = max(0, date_delta.days - self.ANALYSIS_PERIOD)
+            res_table_name = self._sanitize_table_name_for_bq(experiment.normandy_slug)
+
+            time_limits = TimeLimits.for_single_analysis_window(
+                exp.start_date,
+                last_date_full_data,
+                analysis_start_days,
+                self.ANALYSIS_PERIOD,
+                exp.num_dates_enrollment,
             )
+
+            # todo additional experiment specific metrics from Experimenter
+            # todo: use build_query once new version of mozanalysis lands
+            sql = exp._build_query(self.STANDARD_METRICS, time_limits, "normandy", None)
+
+            # todo: add option to append to table; right now the table gets reused
+            self.bq_context.run_query(sql, res_table_name)
