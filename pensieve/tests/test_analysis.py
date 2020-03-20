1f0b0d96c7116c647000e72e3645f442f2d0f78f
Tim D. Smith <tdsmith@mozilla.com>
2020-03-20 09:57:50 -0700

    Add regression test case

--- pensieve/tests/test_analysis.py
+++ pensieve/tests/test_analysis.py
@@ -1,5 +1,6 @@
 import datetime as dt
 from datetime import timedelta
+import json
 import pytz
 import pytest
 
@@ -41,3 +42,41 @@ def test_get_timelimits_if_ready(experiments):
 
     date = dt.datetime(2019, 12, 1, tzinfo=pytz.utc) + timedelta(2)
     assert analysis._get_timelimits_if_ready(experiments[0], date) is None
+
+
+def test_regression_20200320():
+    experiment_json = r"""
+        {
+          "experiment_url": "https://experimenter.services.mozilla.com/experiments/impact-of-level-2-etp-on-a-custom-distribution/",
+          "type": "pref",
+          "name": "Impact of Level 2 ETP on a Custom Distribution",
+          "slug": "impact-of-level-2-etp-on-a-custom-distribution",
+          "public_name": "Impact of Level 2 ETP",
+          "status": "Live",
+          "start_date": 1580169600000,
+          "end_date": 1595721600000,
+          "proposed_start_date": 1580169600000,
+          "proposed_enrollment": null,
+          "proposed_duration": 180,
+          "normandy_slug": "pref-impact-of-level-2-etp-on-a-custom-distribution-release-72-80-bug-1607493",
+          "normandy_id": 906,
+          "other_normandy_ids": [],
+          "variants": [
+            {
+              "description": "",
+              "is_control": true,
+              "name": "treatment",
+              "ratio": 100,
+              "slug": "treatment",
+              "value": "true",
+              "addon_release_url": null,
+              "preferences": []
+            }
+          ]
+        }
+    """  # noqa
+    experiment = Experiment.from_dict(json.loads(experiment_json))
+    analysis = Analysis("test", "test")
+    analysis.run(
+        experiment, current_date=dt.datetime(2020, 3, 19), dry_run=True,
+    )
