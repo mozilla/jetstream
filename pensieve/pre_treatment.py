a7f9e69b763b13b82a8a92684029986cc8f7dd15
Anna Scholtz <anna@scholtzan.net>
2020-04-24 13:44:50 -0700

    Combine statistics and metrics

--- pensieve/pre_treatment.py
+++ pensieve/pre_treatment.py
@@ -1,26 +1,31 @@
+from abc import ABC, abstractmethod
 import attr
 import cattr
 from pandas import DataFrame
 
 
 @attr.s(auto_attribs=True)
-class PreTreatment:
-    """Represents an abstract pre-treatment step applied to data before calculating statistics."""
+class PreTreatment(ABC):
+    """
+    Represents an abstract pre-treatment step applied to data before
+    calculating statistics.
+    """
 
     @classmethod
     def name(cls):
         return __name__  # todo: snake case names?
 
-    def apply(self, df: DataFrame):
-        """Applies the pre-treatment transformation to a DataFrame."""
-        return self.transformation(df)
-
-    def transformation(self, df: DataFrame):
-        raise NotImplementedError("PreTreatment subclasses must override transformation()")
+    @abstractmethod
+    def apply(self, df: DataFrame) -> DataFrame:
+        """
+        Applies the pre-treatment transformation to a DataFrame and returns
+        the resulting DataFrame.
+        """
+        return NotImplemented
 
 
 class RemoveNulls(PreTreatment):
     """Pre-treatment step to remove null rows and columns."""
 
-    def transformation(self, df: DataFrame):
+    def apply(self, df: DataFrame):
         return df.dropna()
