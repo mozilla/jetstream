79e2af97c68ba9326e201aa546d7a469b4751278
Tim D. Smith <tdsmith@mozilla.com>
2020-05-12 09:25:36 -0700

    Add a couple pretreatments and tests (#53)
    * Declare PreTreatment.apply to be abstract
    
    mypy correctly observes that subclasses of an abstract class may be abstract. We'll make doubly sure that we're instantiating a concrete class and ignore the type error.
    
    * Drop indefinites
    
    Add a class that also drops [-Inf, Inf] and a couple of tests.
    
    * Add a pretreatment to censor high values
    
    * Add a log transform pretreatment

--- pensieve/pre_treatment.py
+++ pensieve/pre_treatment.py
@@ -1,7 +1,11 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 import attr
-from pandas import DataFrame
 import re
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
 
 
 @attr.s(auto_attribs=True)
@@ -16,7 +20,7 @@ class PreTreatment(ABC):
         """Return snake-cased name of the statistic."""
         return re.sub(r"(?<!^)(?=[A-Z])", "_", cls.__name__).lower()
 
-    # @abstractmethod
+    @abstractmethod
     def apply(self, df: DataFrame, col: str) -> DataFrame:
         """
         Applies the pre-treatment transformation to a DataFrame and returns
@@ -26,7 +30,39 @@ class PreTreatment(ABC):
 
 
 class RemoveNulls(PreTreatment):
-    """Pre-treatment step to remove null rows and columns."""
+    """Removes rows with null values."""
 
-    def apply(self, df: DataFrame, col: str):
-        return df[col].dropna()
+    def apply(self, df: DataFrame, col: str) -> DataFrame:
+        return df.dropna(subset=[col])
+
+
+class RemoveIndefinites(PreTreatment):
+    """Removes null and infinite values."""
+
+    def apply(self, df: DataFrame, col: str) -> DataFrame:
+        with pd.option_context("mode.use_inf_as_na", True):
+            return df.dropna(subset=[col])
+
+
+@attr.s(auto_attribs=True)
+class CensorHighestValues(PreTreatment):
+    """Removes rows with the highest n% of values."""
+
+    fraction: float = 1 - 1e-5
+
+    def apply(self, df: DataFrame, col: str) -> DataFrame:
+        mask = df[col] < df[col].quantile(self.fraction)
+        return df.loc[mask, :]
+
+
+@attr.s(auto_attribs=True)
+class Log(PreTreatment):
+    base: Optional[float] = 10.0
+
+    def apply(self, df: DataFrame, col: str) -> DataFrame:
+        # Silence divide-by-zero and domain warnings
+        with np.errstate(divide="ignore", invalid="ignore"):
+            result = np.log(df[col])
+            if self.base:
+                result /= np.log(self.base)
+        return df.assign(**{col: result})
