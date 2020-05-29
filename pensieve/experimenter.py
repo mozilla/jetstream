7a971e5adb7a5b08d99f6b1d2bb647bfc8bf8bb7
Tim D. Smith <tdsmith@mozilla.com>
2020-05-29 09:39:02 -0700

    Add ever_launched() filter

--- pensieve/experimenter.py
+++ pensieve/experimenter.py
@@ -62,22 +62,32 @@ class ExperimentCollection:
         cls = type(self)
         return cls([ex for ex in self.experiments if ex.type in type_or_types])
 
+    def ever_launched(self) -> "ExperimentCollection":
+        cls = type(self)
+        return cls([ex for ex in self.experiments if ex.status in ("Complete", "Live")])
+
     def with_slug(self, slug: str) -> "ExperimentCollection":
         cls = type(self)
         return cls([ex for ex in self.experiments if ex.slug == slug or ex.normandy_slug == slug])
 
     def started_since(self, since: dt.datetime) -> "ExperimentCollection":
-        """since should be a tz-aware datetime in UTC."""
-        cls = type(self)
-        return cls([ex for ex in self.experiments if ex.start_date and ex.start_date >= since])
+        """All experiments that ever launched after a given time.
 
-    def end_on_or_after(self, after: dt.datetime) -> "ExperimentCollection":
-        """All live experiments that end on or after the specified date."""
+        since should be a tz-aware datetime."""
         cls = type(self)
         return cls(
             [
                 ex
-                for ex in self.experiments
-                if ex.end_date and ex.end_date >= after and ex.status in ("Live", "Complete")
+                for ex in self.ever_launched().experiments
+                if ex.start_date and ex.start_date >= since
             ]
+        )
+
+    def end_on_or_after(self, after: dt.datetime) -> "ExperimentCollection":
+        """All experiments that ever launched that end on or after the specified time.
+
+        after should be a tz-aware datetime."""
+        cls = type(self)
+        return cls(
+            [ex for ex in self.ever_launched().experiments if ex.end_date and ex.end_date >= after]
         )
