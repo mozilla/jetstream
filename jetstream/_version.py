df3e4638966927bc3d6ee5c4e7da480a9edae062
Tim D. Smith <tdsmith@mozilla.com>
2020-07-15 16:25:42 -0700

    Update references

--- jetstream/_version.py
+++ jetstream/_version.py
@@ -1,11 +1,11 @@
 """
-Provides pensieve/ version information.
+Provides jetstream/ version information.
 """
 
 # This file is auto-generated! Do not edit!
-# Use `python -m incremental.update pensieve/` to change this file.
+# Use `python -m incremental.update jetstream/` to change this file.
 
 from incremental import Version
 
-__version__ = Version("pensieve/", 20, 7, 0)
+__version__ = Version("jetstream/", 20, 7, 0)
 __all__ = ["__version__"]
