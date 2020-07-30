5dc5c7014e18c29165c77b8875efb61f71945cb3
Anna Scholtz <anna@scholtzan.net>
2020-07-30 09:59:50 -0700

    Run multiple comparisons when requested

--- jetstream/tests/test_statistics.py
+++ jetstream/tests/test_statistics.py
@@ -44,3 +44,33 @@ class TestStatistics:
         result = stat.transform(test_data, "asdfasdf", "control").data
         assert [r.point for r in result if r.branch == "treatment"] == [20]
         assert [r.point for r in result if r.branch == "control"] == [10]
+
+    def test_binomial_no_reference_branch(self):
+        stat = Binomial(ref_branch_label=None)
+        test_data = pd.DataFrame(
+            {
+                "branch": ["treatment"] * 10 + ["control"] * 10 + ["foo"] * 10,
+                "value": [False] * 7
+                + [True] * 3
+                + [False] * 5
+                + [True] * 5
+                + [False] * 5
+                + [True] * 5,
+            }
+        )
+        result = stat.transform(test_data, "value")
+
+        branch_results = [r for r in result.data if r.comparison is None]
+        treatment_result = [r for r in branch_results if r.branch == "treatment"][0]
+        control_result = [r for r in branch_results if r.branch == "control"][0]
+        assert treatment_result.point < control_result.point
+        assert treatment_result.point - 0.7 < 1e-5
+
+        difference = [r for r in result.data if r.comparison == "difference"][0]
+        assert difference.point - 0.2 < 1e-5
+        assert difference.lower and difference.upper
+
+        comparison_branches = [r.comparison_to_branch for r in result.data]
+        assert "foo" in comparison_branches
+        assert "control" in comparison_branches
+        assert "treatment" in comparison_branches
