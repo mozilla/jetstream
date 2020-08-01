6b3c5e88becee76d5bffb9ee079e30f525a6936d
Tim D. Smith <tdsmith@mozilla.com>
2020-07-31 18:32:18 -0700

    Fix integration test

--- jetstream/tests/integration/test_featureresolver_integration.py
+++ jetstream/tests/integration/test_featureresolver_integration.py
@@ -1,10 +1,14 @@
+from unittest.mock import Mock
+
 from jetstream.nimbus import Feature, FeatureEventTelemetry, FeatureResolver
 
 
 def test_feature_resolver():
     pip = FeatureResolver.resolve("picture_in_picture")
     assert isinstance(pip, Feature)
-    summaries = pip.to_summaries()
+    fake_config = Mock()
+    fake_config.reference_branch = None
+    summaries = pip.to_summaries("slug", fake_config)
     assert len(summaries)
     assert len(pip.telemetry)
     assert len(
