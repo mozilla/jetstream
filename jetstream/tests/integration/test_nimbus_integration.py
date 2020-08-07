01e676d9407155cb93852f3b28feebab0b5e0002
Tim D. Smith <tdsmith@mozilla.com>
2020-08-07 10:59:06 -0700

    Skip test in CI

--- jetstream/tests/integration/test_nimbus_integration.py
+++ jetstream/tests/integration/test_nimbus_integration.py
@@ -1,12 +1,18 @@
+import os
+
+import pytest
+
 from jetstream.nimbus import ProbeLister
 
 
 class TestNimbusIntegration:
+    @pytest.mark.skipif("CI" in os.environ, reason="CI doesn't have permissions for this table")
     def test_probelister(self):
-        columns = ProbeLister.columns_for_scalar("telemetry.accumulate_unknown_histogram_keys")
-        for process in ("content", "dynamic", "extension", "gpu", "parent", "socket"):
+        columns = ProbeLister.columns_for_scalar("telemetry.discarded.accumulations")
+        for process in ("content", "dynamic", "extension", "gpu", "socket"):
             assert (
-                f"payload.processes.{process}.scalars.telemetry_accumulate_unknown_histogram_keys"
-                in columns
+                f"payload.processes.{process}.scalars.telemetry_discarded_accumulations" in columns
             )
 
+        columns = ProbeLister.columns_for_scalar("telemetry.data_upload_optin")
+        assert columns == ["payload.processes.parent.scalars.telemetry_data_upload_optin"]
