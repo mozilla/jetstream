df1659c000e1558a2d1f5dd0f98f18f1df74b33c
Anna Scholtz <anna@scholtzan.net>
2020-10-20 15:33:07 -0700

    Add CLI tests

--- jetstream/tests/test_cli.py
+++ jetstream/tests/test_cli.py
@@ -1,8 +1,16 @@
-from datetime import date
+from click.testing import CliRunner
+from datetime import date, datetime
+import pytest
+
 from jetstream import cli
+from jetstream.experimenter import Experiment
 
 
 class TestCli:
+    @pytest.fixture
+    def runner(self):
+        return CliRunner()
+
     def test_inclusive_date_range(self):
         start_date = date(2020, 5, 1)
         end_date = date(2020, 5, 1)
@@ -16,3 +24,31 @@ class TestCli:
         assert len(date_range) == 5
         assert date_range[0] == date(2020, 5, 1)
         assert date_range[4] == date(2020, 5, 5)
+
+    def test_get_active_experiments(self, runner):
+        result = runner.invoke(cli.get_active_experiments, ["--start_date=2020-01-01"])
+        assert result.exit_code == 0
+        assert result.output != ""
+
+    def test_analyse_experiment(self, runner):
+        result = runner.invoke(cli.analyse_experiment, [""])
+        assert result.exit_code == 2
+
+        epxeriment = Experiment(
+            experimenter_slug=None,
+            normandy_slug="test",
+            type="v6",
+            status="Live",
+            features=[],
+            branches=[],
+            start_date=datetime(2019, 12, 1),
+            end_date=datetime(2020, 12, 1),
+            proposed_enrollment=14,
+            reference_branch=None,
+            is_high_population=False,
+        )
+        experiment_config = cli._base64_encode(
+            cli.AnalysisRunConfig(datetime(2020, 1, 1), epxeriment, None)
+        )
+        result = runner.invoke(cli.analyse_experiment, [f"--experiment_config={experiment_config}"])
+        assert result.exit_code == 0
