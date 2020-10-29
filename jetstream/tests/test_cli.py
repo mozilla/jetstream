3c2d4730c1be015f88c6ae5da5c0069db760c3a6
Tim D. Smith <tdsmith@mozilla.com>
2020-10-29 09:22:29 -0700

    Add test for trivial case

--- jetstream/tests/test_cli.py
+++ jetstream/tests/test_cli.py
@@ -1,18 +1,49 @@
-from datetime import date
-from jetstream import cli
+import attr
+import datetime as dt
+
+from pytz import UTC
+
+from jetstream import cli, experimenter, external_config
 
 
 class TestCli:
     def test_inclusive_date_range(self):
-        start_date = date(2020, 5, 1)
-        end_date = date(2020, 5, 1)
+        start_date = dt.date(2020, 5, 1)
+        end_date = dt.date(2020, 5, 1)
         date_range = list(cli.inclusive_date_range(start_date, end_date))
         assert len(date_range) == 1
-        assert date_range[0] == date(2020, 5, 1)
+        assert date_range[0] == dt.date(2020, 5, 1)
 
-        start_date = date(2020, 5, 1)
-        end_date = date(2020, 5, 5)
+        start_date = dt.date(2020, 5, 1)
+        end_date = dt.date(2020, 5, 5)
         date_range = list(cli.inclusive_date_range(start_date, end_date))
         assert len(date_range) == 5
-        assert date_range[0] == date(2020, 5, 1)
-        assert date_range[4] == date(2020, 5, 5)
+        assert date_range[0] == dt.date(2020, 5, 1)
+        assert date_range[4] == dt.date(2020, 5, 5)
+
+
+@attr.s(auto_attribs=True)
+class DummyExecutorStrategy(cli.ExecutorStrategy):
+    return_value: bool = True
+
+    def execute(self, worklist):
+        self.worklist = worklist
+        return self.return_value
+
+
+class TestAnalysisExecutor:
+    def test_trivial_case(self):
+        executor = cli.AnalysisExecutor(
+            project_id="project",
+            dataset_id="dataset",
+            date=dt.datetime(2020, 10, 28, tzinfo=UTC),
+            experiment_slugs=[],
+        )
+        strategy = DummyExecutorStrategy
+        success = executor.execute(
+            experiment_getter=experimenter.ExperimentCollection,
+            config_getter=external_config.ExternalConfigCollection,
+            strategy=strategy,
+        )
+        assert success
+        assert strategy.worklist == []
