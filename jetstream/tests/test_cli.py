7fecc244a0148075ee798221259363a368986281
Tim D. Smith <tdsmith@mozilla.com>
2020-10-30 15:16:24 -0700

    Test SerialExecutorStrategy

--- jetstream/tests/test_cli.py
+++ jetstream/tests/test_cli.py
@@ -1,9 +1,51 @@
 import attr
 import datetime as dt
 
+import pytest
 from pytz import UTC
+from unittest.mock import Mock
 
 from jetstream import cli, experimenter, external_config
+
+
+@pytest.fixture
+def cli_experiments():
+    return experimenter.ExperimentCollection(
+        [
+            experimenter.Experiment(
+                experimenter_slug=None,
+                normandy_slug="my_cool_experiment",
+                type="v6",
+                status="Live",
+                branches=[
+                    experimenter.Branch(slug="treatment", ratio=1),
+                    experimenter.Branch(slug="control", ratio=1),
+                ],
+                probe_sets=[],
+                start_date=dt.datetime(2020, 1, 1, tzinfo=UTC),
+                end_date=dt.datetime(2021, 2, 1, tzinfo=UTC),
+                proposed_enrollment=None,
+                reference_branch="control",
+                is_high_population=False,
+            ),
+            experimenter.Experiment(
+                experimenter_slug=None,
+                normandy_slug="distracting_experiment",
+                type="v6",
+                status="Live",
+                branches=[
+                    experimenter.Branch(slug="treatment", ratio=1),
+                    experimenter.Branch(slug="control", ratio=1),
+                ],
+                probe_sets=[],
+                start_date=dt.datetime(2020, 1, 1, tzinfo=UTC),
+                end_date=dt.datetime(2020, 12, 31, tzinfo=UTC),
+                proposed_enrollment=None,
+                reference_branch="control",
+                is_high_population=False,
+            ),
+        ]
+    )
 
 
 class TestCli:
@@ -50,52 +92,16 @@ class TestAnalysisExecutor:
         assert success
         assert strategy.worklist == []
 
-    def test_single_date(self):
+    def test_single_date(self, cli_experiments):
         executor = cli.AnalysisExecutor(
             project_id="project",
             dataset_id="dataset",
             date=dt.datetime(2020, 10, 28, tzinfo=UTC),
             experiment_slugs=["my_cool_experiment"],
         )
-        experiments = experimenter.ExperimentCollection(
-            [
-                experimenter.Experiment(
-                    experimenter_slug=None,
-                    normandy_slug="my_cool_experiment",
-                    type="v6",
-                    status="Live",
-                    branches=[
-                        experimenter.Branch(slug="treatment", ratio=1),
-                        experimenter.Branch(slug="control", ratio=1),
-                    ],
-                    probe_sets=[],
-                    start_date=dt.datetime(2020, 1, 1, tzinfo=UTC),
-                    end_date=dt.datetime(2020, 12, 31, tzinfo=UTC),
-                    proposed_enrollment=None,
-                    reference_branch="control",
-                    is_high_population=False,
-                ),
-                experimenter.Experiment(
-                    experimenter_slug=None,
-                    normandy_slug="distracting_experiment",
-                    type="v6",
-                    status="Live",
-                    branches=[
-                        experimenter.Branch(slug="treatment", ratio=1),
-                        experimenter.Branch(slug="control", ratio=1),
-                    ],
-                    probe_sets=[],
-                    start_date=dt.datetime(2020, 1, 1, tzinfo=UTC),
-                    end_date=dt.datetime(2020, 12, 31, tzinfo=UTC),
-                    proposed_enrollment=None,
-                    reference_branch="control",
-                    is_high_population=False,
-                ),
-            ]
-        )
         strategy = DummyExecutorStrategy("project", "dataset")
         success = executor.execute(
-            experiment_getter=lambda: experiments,
+            experiment_getter=lambda: cli_experiments,
             config_getter=external_config.ExternalConfigCollection,
             strategy=lambda *args, **kwargs: strategy,
         )
@@ -104,52 +110,16 @@ class TestAnalysisExecutor:
         assert strategy.worklist[0][0] == "my_cool_experiment"
         assert strategy.worklist[0][2] == dt.datetime(2020, 10, 28, tzinfo=UTC)
 
-    def test_any_date(self):
+    def test_any_date(self, cli_experiments):
         executor = cli.AnalysisExecutor(
             project_id="project",
             dataset_id="dataset",
             date=cli.All,
             experiment_slugs=["my_cool_experiment"],
         )
-        experiments = experimenter.ExperimentCollection(
-            [
-                experimenter.Experiment(
-                    experimenter_slug=None,
-                    normandy_slug="my_cool_experiment",
-                    type="v6",
-                    status="Live",
-                    branches=[
-                        experimenter.Branch(slug="treatment", ratio=1),
-                        experimenter.Branch(slug="control", ratio=1),
-                    ],
-                    probe_sets=[],
-                    start_date=dt.datetime(2020, 1, 1, tzinfo=UTC),
-                    end_date=dt.datetime(2021, 2, 1, tzinfo=UTC),
-                    proposed_enrollment=None,
-                    reference_branch="control",
-                    is_high_population=False,
-                ),
-                experimenter.Experiment(
-                    experimenter_slug=None,
-                    normandy_slug="distracting_experiment",
-                    type="v6",
-                    status="Live",
-                    branches=[
-                        experimenter.Branch(slug="treatment", ratio=1),
-                        experimenter.Branch(slug="control", ratio=1),
-                    ],
-                    probe_sets=[],
-                    start_date=dt.datetime(2020, 1, 1, tzinfo=UTC),
-                    end_date=dt.datetime(2020, 12, 31, tzinfo=UTC),
-                    proposed_enrollment=None,
-                    reference_branch="control",
-                    is_high_population=False,
-                ),
-            ]
-        )
         strategy = DummyExecutorStrategy("project", "dataset")
         success = executor.execute(
-            experiment_getter=lambda: experiments,
+            experiment_getter=lambda: cli_experiments,
             config_getter=external_config.ExternalConfigCollection,
             strategy=lambda *args, **kwargs: strategy,
             today=dt.datetime(2020, 12, 31, tzinfo=UTC),
@@ -157,3 +127,25 @@ class TestAnalysisExecutor:
         assert success
         assert len(strategy.worklist) == 366
         assert set(w[0] for w in strategy.worklist) == {"my_cool_experiment"}
+
+
+class TestSerialExecutorStrategy:
+    def test_trivial_workflow(self, cli_experiments):
+        fake_analysis = Mock()
+        strategy = cli.SerialExecutorStrategy(
+            "spam", "eggs", fake_analysis, lambda: cli_experiments
+        )
+        strategy.execute([])
+        fake_analysis().run.assert_not_called()
+
+    def test_simple_workflow(self, cli_experiments):
+        fake_analysis = Mock()
+        experiment = cli_experiments.experiments[0]
+        spec = cli.default_spec_for_experiment(experiment)
+        strategy = cli.SerialExecutorStrategy(
+            "spam", "eggs", fake_analysis, lambda: cli_experiments
+        )
+        run_date = dt.datetime(2020, 10, 31, tzinfo=UTC)
+        strategy.execute([(experiment.normandy_slug, spec, run_date)])
+        fake_analysis.assert_called_once_with("spam", "eggs", spec.resolve(experiment))
+        fake_analysis().run.assert_called_once_with(run_date)
