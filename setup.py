8a6a97efe7890cb39d109a781c1f1e8623a8f22d
Tim D. Smith <tdsmith@mozilla.com>
2020-12-01 15:34:59 -0800

    Pin argo-workflows

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     },
     install_requires=[
         "attrs",
-        "argo-workflows",
+        "argo-workflows<4.0",
         "cattrs<1.1",
         "Click",
         "dask[distributed]",
