2ba53ffbf541608075cdec2770b6415083f44688
Tim D. Smith <tdsmith@mozilla.com>
2021-03-17 16:12:00 -0700

    Validate default configs
    It'd be nice to have the same safety that we have in jetstream-config here. :)
    
    Closes #473.

---
+++ jetstream/tests/integration/test_default_configs.py
@@ -0,0 +1,31 @@
+import datetime as dt
+
+import pytest
+from pytz import UTC
+
+import jetstream.analysis
+import jetstream.config
+import jetstream.experimenter
+
+
+class TestDefaultConfigs:
+    @pytest.mark.parametrize("platform_name,platform", jetstream.analysis.PLATFORM_CONFIGS.items())
+    def test_default_configs(self, platform_name, platform):
+        experiment = jetstream.experimenter.Experiment(
+            experimenter_slug="dummy-experiment",
+            normandy_slug="dummy_experiment",
+            type="v6",
+            status="Live",
+            branches=[],
+            end_date=None,
+            reference_branch="control",
+            is_high_population=False,
+            start_date=dt.datetime.now(UTC),
+            proposed_enrollment=14,
+            app_id=platform.validation_app_id,
+            app_name=platform_name,
+        )
+
+        spec = jetstream.config.AnalysisSpec.default_for_experiment(experiment)
+        config = spec.resolve(experiment)
+        jetstream.analysis.Analysis("no project", "no dataset", config).validate()
