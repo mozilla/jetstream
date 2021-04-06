3ae49fd38e018b32321b936b645a3102bf699acf
Anna Scholtz <anna@scholtzan.net>
2021-04-06 15:40:27 -0700

    Move retry_get to utils

--- jetstream/util.py
+++ jetstream/util.py
@@ -1,8 +1,13 @@
+import logging
 import shutil
 import tempfile
+import time
 from contextlib import contextmanager
 from datetime import timedelta
 from pathlib import Path
+from typing import Any
+
+logger = logging.getLogger(__name__)
 
 # based on https://stackoverflow.com/a/22726782
 
@@ -20,3 +25,16 @@ def inclusive_date_range(start_date, end_date):
     """Generator for a range of dates, includes end_date."""
     for n in range(int((end_date - start_date).days) + 1):
         yield start_date + timedelta(n)
+
+
+def retry_get(session, url, max_retries) -> Any:
+    for _i in range(max_retries):
+        try:
+            blob = session.get(url).json()
+            break
+        except Exception:
+            logger.info(f"Error fetching from {url}. Retrying...")
+            time.sleep(1)
+    else:
+        raise Exception(f"Too many retries for {url}")
+    return blob
