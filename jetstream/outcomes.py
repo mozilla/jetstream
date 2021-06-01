8707e4354ae35885921770503e9870bffd65a191
Tim D. Smith <tdsmith@mozilla.com>
2021-05-31 21:41:02 -0700

    Make _OutcomesResolver instances 'immutable'

--- jetstream/outcomes.py
+++ jetstream/outcomes.py
@@ -5,31 +5,28 @@ import attr
 from . import external_config
 
 
-@attr.s(auto_attribs=True)
+@attr.s(auto_attribs=True, frozen=True)
 class _OutcomesResolver:
     """Fetches and resolves external outcome snippet definitions."""
 
-    external_configs: Optional[external_config.ExternalConfigCollection] = attr.ib(None)
+    _external_configs: Optional[external_config.ExternalConfigCollection] = None
 
     @property
     def data(self) -> Dict[str, external_config.ExternalOutcome]:
-
         if data := getattr(self, "_data", None):
             return data
 
-        if self.external_configs is None:
-            self.external_configs = external_config.ExternalConfigCollection.from_github_repo()
-        self._data = {outcome.slug: outcome for outcome in self.external_configs.outcomes}
-        return self._data
-
-    def with_external_configs(
-        self, external_configs: Optional[external_config.ExternalConfigCollection]
-    ) -> "_OutcomesResolver":
-        self.external_configs = external_configs
-        return self
+        external_configs = (
+            self._external_configs or external_config.ExternalConfigCollection.from_github_repo()
+        )
+        object.__setattr__(
+            self, "_data", {outcome.slug: outcome for outcome in external_configs.outcomes}
+        )
+        return self._data  # type: ignore
 
     def resolve(self, slug: str) -> external_config.ExternalOutcome:
         return self.data[slug]
 
 
 OutcomesResolver = _OutcomesResolver()
+OutcomesResolverType = _OutcomesResolver
