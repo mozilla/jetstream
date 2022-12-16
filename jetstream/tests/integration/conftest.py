783920d58e0dc7a6aceb6823fc0a03e319a83b0a
Mike Williams <mwilliams@mozilla.com>
2022-12-16 16:35:20 -0500

    fix #1298: assume multiple analysis_bases

--- jetstream/tests/integration/conftest.py
+++ jetstream/tests/integration/conftest.py
@@ -1,7 +1,6 @@
 import random
 import string
 from pathlib import Path
-from unittest.mock import Mock
 
 import pytest
 from google.api_core.exceptions import NotFound
@@ -17,11 +16,6 @@ def pytest_runtest_setup(item):
         return
     if not item.config.getoption("--integration", False):
         pytest.skip("Skipping integration test")
-
-
-@pytest.fixture(autouse=True)
-def setup(monkeypatch):
-    monkeypatch.setattr("jetstream.metric.Metric.__attrs_post_init__", Mock())
 
 
 @pytest.fixture
