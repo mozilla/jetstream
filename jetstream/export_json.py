2a23149a5c5eeb7b3b1e3e81f611d09c958bd2fa
Mike Williams <102263964+mikewilli@users.noreply.github.com>
2023-05-19 15:12:51 -0400

    fix #1748: ignore gcp api exception if table not found during export (#1749)

--- jetstream/export_json.py
+++ jetstream/export_json.py
@@ -9,6 +9,7 @@ import cattr
 import google.cloud.bigquery as bigquery
 import google.cloud.storage as storage
 import smart_open
+from google.cloud.exceptions import BadRequest
 from metric_config_parser.metric import AnalysisPeriod
 
 from jetstream import bq_normalize_name
@@ -62,15 +63,24 @@ def _export_table(
     storage_client: storage.Client,
 ):
     """Export a single table or view to GCS as JSON."""
-    # since views cannot get exported directly, write data into a temporary table
-    job = client.query(
-        f"""
-        SELECT *
-        FROM {dataset_id}.{table}
-    """
-    )
+    try:
+        # since views cannot get exported directly, write data into a temporary table
+        job = client.query(
+            f"""
+            SELECT *
+            FROM {dataset_id}.{table}
+        """
+        )
 
-    job.result()
+        job.result()
+    except BadRequest as e:
+        if "does not match any table" in e.message:
+            logger.error(
+                f"google.cloud.exceptions.BadRequest: {e.args[0]}. Skipping query and export..."
+            )
+            return
+        else:
+            raise e
 
     # add a random string to the identifier to prevent collision errors if there
     # happen to be multiple instances running that export data for the same experiment
