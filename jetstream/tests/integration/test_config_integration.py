c8846547e6cdfa7714055f7b2be10b84f4a0f580
Daniel Berry <dberry@mozilla.com>
2024-05-21 14:56:24 -0700

    error message

--- jetstream/tests/integration/test_config_integration.py
+++ jetstream/tests/integration/test_config_integration.py
@@ -356,6 +356,6 @@ class TestConfigIntegration:
 
         with pytest.raises(
             ValueError,
-            match="Covariate adjustment must be done using a pre-treatment analysis period (one of preenrollment_week, preenrollment_days28)",  # noqa: E501",
+            match="Covariate adjustment must be done using a pre-treatment analysis period (one of ['preenrollment_week', 'preenrollment_days28'])",  # noqa: E501",
         ):
             Summary.from_config(summary, 7).statistic
