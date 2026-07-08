2d43e131a293c09806e097afab3ec2b1d58d55b0
Mike Williams <mwilliams@mozilla.com>
2026-07-08 17:40:39 -0400

    remove extra var

--- jetstream/analysis.py
+++ jetstream/analysis.py
@@ -886,7 +886,6 @@ class Analysis:
         )
 
         if not self.bigquery.column_exists_in_table(covariate_table_name, covariate_metric_name):
-            normalized_slug = bq_normalize_name(self.config.experiment.normandy_slug)
             log_msg = (
                 f"Covariate adjustment table {covariate_table_name} does not exist "
                 f"(or `{covariate_metric_name}` not found in table), "
