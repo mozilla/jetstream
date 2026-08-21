9c80bce8bf4d49c92b82967a2aeb3281e39b9750
Mike Williams <mwilliams@mozilla.com>
2026-08-21 10:31:20 -0400

    chore: make argo client code more resilient to unexpected workflow structure

--- jetstream/tests/test_argo.py
+++ jetstream/tests/test_argo.py
@@ -1,70 +1,45 @@
 from textwrap import dedent
+from unittest import mock
 
 import yaml
 
 from jetstream import argo
 from jetstream.cli import ArgoExecutorStrategy
 
+MINIMAL_WORKFLOW = dedent(
+    """
+    apiVersion: argoproj.io/v1alpha1
+    kind: Workflow
+    metadata:
+        generateName: jetstream-
+    spec:
+        entrypoint: jetstream
+        arguments:
+            parameters:
+            - name: date
+    """
+)
+
 
 class TestArgo:
     def test_apply_parameters(self):
-        config_str = dedent(
-            """
-            apiVersion: argoproj.io/v1alpha1
-            kind: Workflow
-            metadata:
-                generateName: jetstream-
-            spec:
-                entrypoint: jetstream
-                arguments:
-                    parameters:
-                    - name: date
-            """
-        )
-
-        manifest = yaml.safe_load(config_str)
+        manifest = yaml.safe_load(MINIMAL_WORKFLOW)
         updated_manifest = argo.apply_parameters(manifest, {"date": "2020-01-01"})
         assert updated_manifest["spec"]["arguments"]["parameters"][0]["name"] == "date"
         assert updated_manifest["spec"]["arguments"]["parameters"][0]["value"] == "2020-01-01"
 
     def test_apply_parameters_overwrite(self):
-        config_str = dedent(
-            """
-            apiVersion: argoproj.io/v1alpha1
-            kind: Workflow
-            metadata:
-                generateName: jetstream-
-            spec:
-                entrypoint: jetstream
-                arguments:
-                    parameters:
-                    - name: date
-                      value: 2020-12-12
-            """
-        )
+        manifest = yaml.safe_load(MINIMAL_WORKFLOW)
+        manifest["spec"]["arguments"]["parameters"][0]["value"] = "2020-12-12"
 
-        manifest = yaml.safe_load(config_str)
         updated_manifest = argo.apply_parameters(manifest, {"date": "2020-01-01"})
         assert updated_manifest["spec"]["arguments"]["parameters"][0]["name"] == "date"
         assert updated_manifest["spec"]["arguments"]["parameters"][0]["value"] == "2020-01-01"
 
     def test_apply_parameters_add(self):
-        config_str = dedent(
-            """
-            apiVersion: argoproj.io/v1alpha1
-            kind: Workflow
-            metadata:
-                generateName: jetstream-
-            spec:
-                entrypoint: jetstream
-                arguments:
-                    parameters:
-                    - name: date
-                      value: 2020-12-12
-            """
-        )
+        manifest = yaml.safe_load(MINIMAL_WORKFLOW)
+        manifest["spec"]["arguments"]["parameters"][0]["value"] = "2020-12-12"
 
-        manifest = yaml.safe_load(config_str)
         updated_manifest = argo.apply_parameters(manifest, {"date": "2020-01-01", "slug": "test"})
         assert updated_manifest["spec"]["arguments"]["parameters"][0]["name"] == "date"
         assert updated_manifest["spec"]["arguments"]["parameters"][0]["value"] == "2020-01-01"
@@ -89,4 +64,40 @@ class TestArgo:
                 updated_manifest["spec"]["arguments"]["parameters"][0]["value"]
                 == '[{"date": "2020-01-01", "slug": "a", "image_hash": "abc"}, '
                 + '{"date": "2020-01-02", "slug": "b", "image_hash": "abc"}]'
+            )
+
+    def test_submit_workflow_status_without_nodes(self, tmp_path):
+        workflow_file = tmp_path / "workflow.yaml"
+        workflow_file.write_text(MINIMAL_WORKFLOW)
+
+        created_workflow = {
+            "metadata": {"namespace": "argo", "name": "jetstream-abc"},
+            "status": {"phase": "Succeeded"},
+        }
+        with mock.patch.object(argo.ArgoApi, "create_workflow", return_value=created_workflow):
+            assert (
+                argo.submit_workflow(
+                    "project", "zone", "cluster", workflow_file, {"date": "2020-01-01"}
+                )
+                is True
+            )
+
+    def test_submit_workflow_nodes_failure_detected(self, tmp_path):
+        workflow_file = tmp_path / "workflow.yaml"
+        workflow_file.write_text(MINIMAL_WORKFLOW)
+
+        created_workflow = {
+            "metadata": {"namespace": "argo", "name": "jetstream-abc"},
+            "status": {
+                "nodes": {
+                    "node1": {"name": "step-one(0)", "type": "Pod", "phase": "Failed"},
+                }
+            },
+        }
+        with mock.patch.object(argo.ArgoApi, "create_workflow", return_value=created_workflow):
+            assert (
+                argo.submit_workflow(
+                    "project", "zone", "cluster", workflow_file, {"date": "2020-01-01"}
+                )
+                is False
             )
